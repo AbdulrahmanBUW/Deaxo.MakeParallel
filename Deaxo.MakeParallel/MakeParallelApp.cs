6ea39b0e08cfb7a33ad9db1df422dbca666aeac3
AbdulrahmanBUW <abdul.rahman@uni-weimar.de>
2025-09-18 16:50:33 +0200

    Icon Fixed

--- Deaxo.MakeParallel/MakeParallelApp.cs
+++ Deaxo.MakeParallel/MakeParallelApp.cs
@@ -1,5 +1,10 @@
 ﻿using Autodesk.Revit.UI;
 using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using System.Drawing;
 
 namespace Deaxo.MakeParallel
 {
@@ -18,7 +23,7 @@ namespace Deaxo.MakeParallel
                 RibbonPanel panel = application.CreateRibbonPanel(tabName, "Alignment Tools");
 
                 // PushButton data
-                string assemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string assemblyPath = Assembly.GetExecutingAssembly().Location;
                 PushButtonData buttonData = new PushButtonData(
                     "MakeParallel",
                     "Make\nParallel",
@@ -29,7 +34,11 @@ namespace Deaxo.MakeParallel
                     ToolTip = "Make two elements parallel by rotating the second element to match the first element's direction in XY plane",
                 };
 
-                // Add button to panel (without icons for now - you can add them later)
+                // Load large and small icons using GDI → WPF conversion
+                buttonData.LargeImage = LoadBitmapFromEmbeddedResource("Deaxo.MakeParallel.Resources.parallel32.png");
+                buttonData.Image = LoadBitmapFromEmbeddedResource("Deaxo.MakeParallel.Resources.parallel16.png");
+
+                // Add button to panel
                 panel.AddItem(buttonData);
 
                 return Result.Succeeded;
@@ -44,6 +53,32 @@ namespace Deaxo.MakeParallel
         public Result OnShutdown(UIControlledApplication application)
         {
             return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Loads an embedded PNG and converts it to BitmapSource for Revit ribbon.
+        /// </summary>
+        /// <param name="resourceName">Namespace + folder + filename</param>
+        /// <returns>BitmapSource or null</returns>
+        private BitmapSource LoadBitmapFromEmbeddedResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null) return null;
+
+                using (var bmp = new Bitmap(stream))
+                {
+                    var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(
+                        bmp.GetHbitmap(),
+                        IntPtr.Zero,
+                        System.Windows.Int32Rect.Empty,
+                        BitmapSizeOptions.FromWidthAndHeight(bmp.Width, bmp.Height)
+                    );
+                    bitmapSource.Freeze();
+                    return bitmapSource;
+                }
+            }
         }
     }
 }
